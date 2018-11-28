6f20c98f1fc2828733fffa9e6998204a78fc7f55
Eran Shiber <44651526+EranShiber@users.noreply.github.com>
2018-11-29 01:00:36 +0200

    Update search.js

--- search.js
+++ search.js
@@ -1,40 +1,42 @@
 const path = require('path');
 const fs = require('fs');
 
-
+let results = 0;
 let ext = process.argv[2];
-let text = process.argv[3];
-let res = 0;
-if (ext && text) {
-    fromDir('./', ext, text);
-    if (res === 0) {
-        console.log("No file was found");
-    }
-    else {
-        console.log(results + " Result/s");
-    }
+let sTerm = process.argv[3];
+if(ext && sTerm) {
+  fromDir('./',ext,sTerm);
+  if( results===0) {
+      console.log("No file was found");
+  }
+  else { console.log(results+" Result/s");
+
+  }
+
 } else {
-    console.log("USAGE: node search [EXT] [TEXT]");
+  console.log("USAGE: node search [EXT] [TEXT]");
 }
 
-function fromDir(startPath, ext, text) {
-    if (!fs.existsSync(startPath)) {
-        console.log("no dir ", startPath);
+function fromDir(startPath,ext,sTerm){
+    if (!fs.existsSync(startPath)){
+        console.log("no dir ",startPath);
         return;
     }
-    var files = fs.readdirSync(startPath);
-    for (var i = 0; i < files.length; i++) {
-        var filename = path.join(startPath, files[i]);
+
+    var files=fs.readdirSync(startPath);
+
+    for(var i=0;i<files.length;i++){
+        var filename=path.join(startPath,files[i]);
         var stat = fs.lstatSync(filename);
-        if (stat.isDirectory()) {
-            fromDir(filename, ext, sTerm); //recurse
+        if (stat.isDirectory()){
+            fromDir(filename,ext, sTerm); //recurse
         }
-        else if (filename.indexOf(ext) >= (filename.length - 3)) {
-            var content = fs.readFileSync(filename, "utf8");
-            if (content.includes(text)) {
-                console.log("-Found - " + __dirname + "\\" + filename)
-                results++;
-            }
-        };
-    };
-};
+        else if (filename.indexOf(ext)>=(filename.length-3)) {
+          var content = fs.readFileSync(filename,"utf8");
+  if(content.includes(sTerm)) {
+console.log("-Found - "+__dirname+"\\"+filename)
+results++;
+          }
+       };
+      };
+   };
